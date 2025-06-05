d8f7e273c8de9090d21dbfc2c9f5dc6fc74ceae1
luchlima <lucashlima78@gmail.com>
2025-06-05 11:01:11 -0300

    feat: Inicio Projeto

---
+++ src/test/java/com/faculdade/alunos/AlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faculdade.alunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
